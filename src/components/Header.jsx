ed135c3a6a6c6de8f7b4b287cea09d75ca46bdbb
vairavan-ven <vairavan.ven@gmail.com>
2024-01-26 10:51:08 +1100

    added pfp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
-// Header.jsx
+// Heasder.jsx
 import React from 'react';
-import '../styling/header.css'
+import '../styling/header.css';
+import githubPfp from '../assets/github-pfp.jpeg'; // Import the image
 
 const Header = () => {
   return (
     <header>
+      <img src={githubPfp} alt="GitHub Profile" style={{ width: '50px', height: '50px', borderRadius: '50%' }} />
       <h1>Vairavan</h1>
     </header>
   );
